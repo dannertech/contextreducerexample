4c80dd8e69d9ea8906daee40b8f3697661cd31cc
Diamonique Danner <danner@danner.tech>
2022-03-22 14:52:47 -0400

    added missing export

---
+++ src/Components/Cart.js
@@ -0,0 +1,9 @@
+import React, {useState, useReducer, useContext} from 'react';
+
+const Cart = () => {
+    return(
+        <div>
+            <h1>This is the cart component</h1>
+        </div>
+    )
+}
